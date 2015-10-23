c309cb53ff07ab3b267734254d721765090e0e95
Almir Campos <ac@javasd.com>
2015-10-23 14:11:07 -0700

    Initial commit

---
+++ src/com/e1c/prt/Main.java
@@ -0,0 +1,10 @@
+package com.e1c.prt;
+
+/**
+ * Created by almircampos on 10/23/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, there!");
+    }
+}
